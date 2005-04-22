da54bd3c5bdd2ffaac5be077080ec9ed4b51263b
pixel <pixel@bf8e388e-adfb-0310-9433-acdc3088d20d>
2005-04-22 21:24:33 +0000

    Per popular request...

--- isjpcm/ee/src/sjpcm.h
+++ isjpcm/ee/src/sjpcm.h
@@ -31,15 +31,18 @@ extern "C" {
 #define	SJPCM_INIT		0x02
 #define SJPCM_PLAY		0x03
 #define SJPCM_PAUSE		0x04
-#define SJPCM_SETVOL	0x05
-#define SJPCM_ENQUEUE	0x06
-#define SJPCM_CLEARBUFF	0x07
+#define SJPCM_SETVOL		0x05
+#define SJPCM_ENQUEUE		0x06
+#define SJPCM_CLEARBUFF		0x07
 #define SJPCM_QUIT		0x08
-#define SJPCM_GETAVAIL  0x09
-#define SJPCM_GETBUFFD  0x10
+#define SJPCM_GETAVAIL  	0x09
+#define SJPCM_GETBUFFD  	0x10
+#define SJPCM_SETNUMBLOCKS	0x11
+#define SJPCM_SETTHRESHOLD	0x12
 
 void SjPCM_Puts(char *format, ...);
 int SjPCM_Init(int sync);
+int SjPCM_InitEx(int sync, int num_blocks);
 void SjPCM_Enqueue(short *left, short *right, int size, int wait);
 void SjPCM_Play();
 void SjPCM_Pause();
@@ -47,6 +50,7 @@ void SjPCM_Setvol(unsigned int volume);
 void SjPCM_Clearbuff();
 int SjPCM_Available();
 int SjPCM_Buffered();
+void SjPCM_SetCallback(unsigned int threshold, void (*cb)(void));
 void SjPCM_Quit();
 
 #ifdef __cplusplus
