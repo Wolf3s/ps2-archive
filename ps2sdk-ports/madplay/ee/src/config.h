6cf9596a8dda2eab4b8de9df39aed0e339c1f1d7
oopo <oopo@bf8e388e-adfb-0310-9433-acdc3088d20d>
2006-02-21 00:19:00 +0000

    ps2client v3.0.0 release tag

--- ps2sdk-ports/madplay/ee/src/config.h
+++
@@ -1,3 +0,0 @@
-#define AUDIO_DEFAULT audio_sjpcm
-/* #define FPM_DEFAULT */
-#define FPM_64BIT
--- sioshell/ymodem.h
+++
@@ -1,7 +0,0 @@
-#ifndef __YMODEM_H__
-#define __YMODEM_H__
-
-void ymodem_send_data(const u8* data, u32 size, const char* filename);
-int ymodem_recv_data(u8* data, int max_size);
-
-#endif
